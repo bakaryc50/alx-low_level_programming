cc1e2d0c17192b1b2e433ece82d05fa9d30f0fb9
Bakaryc50 <bakaryc50@gmail.com>
2021-06-17 16:19:12 -0700

    Adding: 0x00 hello worl scripts and C programming project

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,6 +6,6 @@
  */
 int main(void)
 {
-	fwrite(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 13);
+	fprintf("and that piece of art is useful\" - Dora Korpar, 2015-10-19", 13);
 	return (0);
 }
