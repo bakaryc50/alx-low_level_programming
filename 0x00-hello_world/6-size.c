d05075535055cefe61bde1c0d827ee67d91dba56
Bakaryc50 <bakaryc50@gmail.com>
2021-06-17 13:57:33 -0700

    Adding: 0x00 hello world project C programming and scripts

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,6 +6,11 @@
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+
+        printf("Size of a char: %d byte(s)", sizeof(char));
+        printf("Size of an int: %d byte(s)", sizeof(int));
+        printf("Size of a long int: %d byte(s)", sizeof(long int));
+	printf("Size of a long long int: %d byte(s)", sizeof(long long int));
+	printf("Size of a float: %d byte(s)", sizeof(float));
 	return (0);
 }
