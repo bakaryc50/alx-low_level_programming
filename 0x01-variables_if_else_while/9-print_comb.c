db936b037caa792dc0c18b96f1f13d7688cdd678
Bakaryc50 <bakaryc50@gmail.com>
2021-06-18 06:20:39 -0700

    Adding : 0x01 variable if else while project

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+	char ch;
+
+	n = 0;
+	while (n <= 9)
+	{
+		putchar(n % 10 + '0');
+		putchar(',');
+		putchar(' ');
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
