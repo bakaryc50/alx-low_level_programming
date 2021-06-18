c475464d6d7e2ad207f13e6db206db0ab43d81c2
Bakaryc50 <bakaryc50@gmail.com>
2021-06-18 11:37:34 -0700

    Adding: 0x01 variables if else while project

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+	int m;
+	int k;
+
+	for (n = 0; n < 8; n++)
+	{
+		for (m = 0; m < 9; m++)
+		{
+			for (k = 0; k <= 9; k++)
+			{
+				if ((n != m && n!= k) && (n < m && m < k))
+				{
+					putchar(n % 10 + '0');
+					putchar(m % 10 + '0');
+					putchar(k % 10 + '0');
+					if (n == 7 && m == 8 && k == 9)
+					{
+					}
+					else
+					{
+						putchar(',');
+						putchar(' ');
+					}
+
+				}
+			}
+
+		}
+	}
+	printf("\n");
+	return (0);
+}
