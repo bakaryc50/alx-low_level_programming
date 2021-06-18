74dc6b54b068ecca2c4723e77947effbc2d5ce90
Bakaryc50 <bakaryc50@gmail.com>
2021-06-18 03:19:31 -0700

    Adding: 0x01 variables if else while project

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,15 +17,15 @@ int main(void)
 
 	if (ltd > 0)
 	{
-		printf("Last digit of %d is %d and is greater than 5", n, ltd);
+		printf("Last digit of %d is %d and is greater than 5\n", n, ltd);
 	}
 	else if (ltd == 0)
 	{
-		printf("Last digit of %d is %d and is 0", n, ltd);
+		printf("Last digit of %d is %d and is 0\n", n, ltd);
 	}
 	else
 	{
-		printf("Last digit of %d is %d and is less than 6 and not 0 ", n, ltd);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, ltd);
 	}
 	return (0);
 }
