70c4c628c81ba439690c28097671eee3961227f9
Bakaryc50 <bakaryc50@gmail.com>
2021-06-22 11:48:29 -0700

    Adding : 0x02 functions nested loops project

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -8,16 +8,19 @@
  */
 int main(void)
 {
+	char c;
+	
 	_putchar('H');
-	_putchar('o');
-	_putchar('l');
-	_putchar('b');
-	_putchar('e');
-	_putchar('r');
-	_putchar('t');
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if (c == 'o' || c == 'l' || c == 'b' || c == 'e' || c == 'r' || c == 't')
+		{
+			_putchar(c);
+		}
+	}
 	_putchar('o');
 	_putchar('n');
-
+	
 	_putchar('\n');
 	return (0);
 }
