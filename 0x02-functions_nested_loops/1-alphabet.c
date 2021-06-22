824f2c30286a1fce70c32b102eca6d90b76d4de6
Bakaryc50 <bakaryc50@gmail.com>
2021-06-22 13:35:23 -0700

    Adding : 0x02 functions nested loops project

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "_putchar.c"
+
+/**
+ * _1-alphabet - print the alphabet in lower case
+ * 
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+void print_alphabet(void)
+{
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		-putchar(i);
+	}
+}
