bd3c629d7cec9c1388cd1adb3b22a72a6985d4a4
Bakaryc50 <bakaryc50@gmail.com>
2021-06-22 09:58:39 -0700

    Adding : 0x02 functions nested loops project

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,13 @@
+#include <holberton.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	my_printf("Holberton");
+	printf("\n");
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/holberton.h
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int my_printf(string str);
+my_printf(str)
+{
+	printf(str);
+}
