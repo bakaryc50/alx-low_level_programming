e5475b4c722fc9fa8ee720cdd3ff175702df1ffa
Bakaryc50 <bakaryc50@gmail.com>
2021-06-23 12:08:09 -0700

    Adding : 0x02 function nested loops project

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -11,25 +11,9 @@ int _isalpha(int c)
 {
 	int r;
 
-	if (c >= 'a' && c <= 'z')
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 	{
 		r = 1;
-	}
-	else if (c >= 'A' && c <= 'Z')
-	{
-		r = 1;
-	}
-	else if (c >= 0 && c <= 1999)
-	{
-		r = 1;
-	}
-	else if (c >= (0 + '0') && c <= (1999 + '0'))
-	{
-		r = 0;
-	}
-	else if (c == ';' || c == '&')
-	{
-		r = 0;
 	}
 	else
 	{
