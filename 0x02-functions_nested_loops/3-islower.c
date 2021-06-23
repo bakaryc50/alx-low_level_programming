351277ab0761d0e2542643d46da5d18ba3b01199
Bakaryc50 <bakaryc50@gmail.com>
2021-06-23 11:30:03 -0700

    Adding : 0x02 function nested loops project

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -8,16 +8,13 @@ int _islower(int c)
 	int i;
 	int r;
 
-	for (i = 'a'; i <= 'z'; i++)
+	if (c >= 'a' && c <= 'z')
 	{
-		if (c == i + '0'')
-		{
-			r = 1;
-		}
-		else
-		{
-			r = 0;
-		}
+		r = 1;
+	}
+	else
+	{
+		r = 0;
 	}
 	return r;
 }
