6590557a205751477c4acc8b83eedd3f41c892af
Bakaryc50 <bakaryc50@gmail.com>
2021-06-23 12:27:05 -0700

    Adding : 0x02 function nested loops project

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -15,17 +15,17 @@ int print_sign (int n)
 	if (n > 0)
 	{
 		r = 1;
-		putchar('+' + '0');
+		_putchar('+');
 	}
 	else if (n == 0)
 	{
 		r = 0;
-		putchar('0' + '0');
+		_putchar('0');
 	}
 	else
 	{
 		r = -1;
-		putchar('-');
+		_putchar('-');
 	}
 	return (r);
 }
