53d34b656587a80b74fb4e99a3411e573a48bb42
Bakaryc50 <bakaryc50@gmail.com>
2021-06-24 10:57:46 -0700

    Adding: 0x02 function nested loop project

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * print_last_digit - print the last digit of a number
+ * @n: is the parameter of the print_last_digit function
+ *
+ * Return: the value of the last digit
+ */
+
+int print_last_digit(int n)
+{
+
+	return (n % 10);
+}
+
