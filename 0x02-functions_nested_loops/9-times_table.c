1f40b88048e6d217e7ead570150611c5488ccfc2
Bakaryc50 <bakaryc50@gmail.com>
2021-06-26 10:42:24 -0700

    Adding: times_table fucntion nested loops project

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,7 +13,8 @@ void times_table(void)
 	{
 		for (times = 0; times <= 9; times++)
 		{
-			_putchar(n * times + '0');
+			n = n * times;
+			_putchar(n + '0');
 			_putchar(',');
 		}
 		_putchar('\n');
