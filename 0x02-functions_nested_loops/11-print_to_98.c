a7348cf78d4452fa880efd7ea33dd8688c0a0767
Bakaryc50 <bakaryc50@gmail.com>
2021-06-26 19:16:17 -0700

    Adding : print_to_98 function nested loop project

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+#include <stdio>
+
+/**
+ * print_to__98 - print all natural number from n to 98
+ * @n: is the parameter to be passed
+ */
+
+int print_to_98(int n)
+{
+	int i;
+
+	if (n < 98)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			printf("%d", n);
+			if (n != 98)
+				printf(", ");
+		}
+	}
+	else if(n == 98)
+	{
+		printf("%d", n);
+	}
+	else
+	{
+		for (i = n; >= 98; i--)
+		{
+			printf("%d", n);
+			if (n != 98)
+				printf(", ");
+		}
+	}
+}
+
