d1b71739d648f6d6d8327840bcf0f6758a568cfb
Bakaryc50 <bakaryc50@gmail.com>
2021-06-26 21:19:41 -0700

    Adding : print_times_table function nested loop project

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,10 +10,10 @@ void print_times_table(int n)
 	int t;
 
 	if (n > 15 || n < 0)
-		return ;
-	for (t = 0; t < n+1; t++)
+		return;
+	for (t = 0; t < n + 1; t++)
 	{
-		for (times = 0; times < n+1; times++)
+		for (times = 0; times < n + 1; times++)
 		{
 			if (times == 0)
 			{
@@ -21,8 +21,8 @@ void print_times_table(int n)
 			}
 			else
 			{
-				_putchar((t * times < 100)? ' ' : ('0' + (t * times) / 100));
-				_putchar((t * times < 10)? ' ' : ('0' + (t * times) / 10 % 10));
+				_putchar((t * times < 100) ? ' ' : ('0' + (t * times) / 100));
+				_putchar((t * times < 10) ? ' ' : ('0' + (t * times) / 10 % 10));
 				_putchar('0' + (t * times) % 10);
 			}
 			if (times != n)
