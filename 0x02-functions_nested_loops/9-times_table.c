25bcc6be69a7b79ef7597b8d50fe974543b566ed
Bakaryc50 <bakaryc50@gmail.com>
2021-06-26 18:06:56 -0700

    Fix : times_table function nested loop project

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,64 +8,21 @@ void times_table(void)
 {
 	int times;
 	int n;
-	int r;
 
 	for (n = 0; n < 10; n++)
 	{
 		for (times = 0; times < 10; times++)
 		{
-			r = n * times;
-			if (r < 10)
-			{	
-				if (times == 0)
-				{
-					_putchar(r + '0');
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-				}	
-				else if (times >= 2)
-				{
-					_putchar(' ');
-					_putchar(r + '0');
-					if (times == 9)
-					{
-						continue;
-					}
-					else
-					{
-						_putchar(',');
-						_putchar(' ');
-					}
-				}
-				else
-				{
-					_putchar(r + '0');
-					if (times == 9)
-					{
-						continue;
-					}
-					else
-					{
-						_putchar(',');
-						_putchar(' ');
-					}
-				}
-			}
+			if (times == 0)
+				;
+			else if ((n * times) < 10)
+				_putchar(' ');
 			else
-			{
-				_putchar(r / 10 + '0');
-				_putchar(r % 10 + '0');
-				if (times == 9)
-				{
-					continue;
-				}
-				else
-				{
-					_putchar(',');
-					_putchar(' ');
-				}
-			}
+				_putchar('0' + (n * times) / 10);
+			_putchar('0' + (n * times) % 10);
+			if (times < 9)
+				_putchar(',');
+			_putchar(' ');
 		}
 		_putchar('\n');
 	}
