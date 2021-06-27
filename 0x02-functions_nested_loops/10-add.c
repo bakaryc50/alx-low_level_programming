1855eaf310e4df2d70e5391dc46ae68c7ca6669b
Bakaryc50 <bakaryc50@gmail.com>
2021-06-26 18:33:42 -0700

    Adding : add numbers function nested loop project

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "holberton.c"
+
+/**
+ * add - adds two integers and returns the result
+ * @n1, @n2 : are parameters
+ *
+ * Return: the result.
+ */
+int add(int n1, int n2)
+{
+	return ('0' + (n1 + n2));
+}
