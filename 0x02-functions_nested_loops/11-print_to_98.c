e126fb4116189c4dc84acac65f0b11cb83f9fab7
Bakaryc50 <bakaryc50@gmail.com>
2021-06-26 19:27:17 -0700

    Adding : print_to_98 function nested loop project

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -14,8 +14,8 @@ void print_to_98(int n)
 	{
 		for (i = n; i <= 98; i++)
 		{
-			printf("%d", n);
-			if (n != 98)
+			printf("%d", i);
+			if (i != 98)
 				printf(", ");
 		}
 	}
@@ -27,8 +27,8 @@ void print_to_98(int n)
 	{
 		for (i = n; i>= 98; i--)
 		{
-			printf("%d", n);
-			if (n != 98)
+			printf("%d", i);
+			if (i != 98)
 				printf(", ");
 		}
 	}
