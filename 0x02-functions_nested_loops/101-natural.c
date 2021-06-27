720b6a9de1d1f0109f6b99451e99fbd6fdde0304
Bakaryc50 <bakaryc50@gmail.com>
2021-06-26 21:43:30 -0700

    Adding : print_sum_multiple of 3 or 5 function nested loop project

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * print_sum_multiple_35 - print the sum of all the multiples of 3 or 5 below 1024
+ */
+int print_sum_multiple_35(void)
+{
+	int sum;
+	int i;
+
+	for (i = 3; i < 1024; i++)
+	{
+		if ((i % 3 == 0) || (i % 5 == 0))
+		{
+			sum = sum + i;
+		}
+	}
+	printf("%d", sum);
+	printf("\n");
+}
