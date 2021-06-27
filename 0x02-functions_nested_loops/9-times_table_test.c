674a7671f883531d29385fe6c3b4b6315b8e094e
Bakaryc50 <bakaryc50@gmail.com>
2021-06-26 18:41:46 -0700

    Adding : add numbers function nested loop project

---
+++ 0x02-functions_nested_loops/9-times_table_test.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ * times_table - prints the 9 times table, starting with 0
+ */
+
+void times_table(void)
+{
+	int times;
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		for (times = 0; times < 10; times++)
+		{
+			if (times == 0)
+				;
+			else if ((n * times) < 10)
+				_putchar(' ');
+			else
+				_putchar('0' + (n * times) / 10);
+			_putchar('0' + (n * times) % 10);
+			if (times < 9)
+				_putchar(',');
+			_putchar(' ');
+		}
+		_putchar('\n');
+	}
+}
