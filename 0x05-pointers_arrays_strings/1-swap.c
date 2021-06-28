dc9f8b039114d39d00243ba801870d160b2e6f3b
Bakaryc50 <bakaryc50@gmail.com>
2021-06-28 11:32:08 -0700

    First attempt: commiting 0x05-pointers_arrays_strings/1-swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * swap_int - swaps the values of two integers
+ * @a: first pointer parameter to an int
+ * @b: second pointer parameter to an int
+ */
+void swap_int(int *a, int *b)
+{
+	int *tmp;
+
+	*tmp = &a;
+	*a = &b;
+	*b = &tmp;
+}
