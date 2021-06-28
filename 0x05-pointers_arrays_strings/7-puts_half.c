5cb39b8a9f5fbb82e702cbfd6cde221bed93f25a
Bakaryc50 <bakaryc50@gmail.com>
2021-06-28 13:46:33 -0700

    Fix attempt: commiting 0x05-pointers_arrays_strings/7-puts_half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * puts_half - print the second half of a string
+ * @str: the string to print
+ *
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	int len;
+
+	for (len = 0; *(str + len); ++len)
+		;
+	for (str += (len + 1) / 2; *str; ++str)
+		_putchar(*str);
+	_putchar('\n');
+
+}
