0418594a501dd204cbfbbbaf47065008aa29ba5e
Bakaryc50 <bakaryc50@gmail.com>
2021-07-01 05:35:06 -0700

    First attempt: commiting 0x06-pointers_arrays_strings/0-strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,6 +9,21 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	*dest = dest + src;
-	return (*dest);
+	char *dest_end = dest;
+	int src_len = 0;
+
+	while (*dest_end)
+		++dest_end;
+
+	while (src[src_len])
+		++src_len;
+
+	if (src + src_len < dest || dest_end + src_len < src)
+	{
+		do {
+			*dest_end++ = *src++;
+		} while (src_len--);
+	}
+
+	return (dest);
 }
