88ab45416744a05b32fe2e58dab2401de513eaa2
Bakaryc50 <bakaryc50@gmail.com>
2021-07-01 08:35:41 -0700

    First attempt: commiting 0x06-pointers_arrays_strings/4-reverse_array

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * reverse_array - reverse an array of integers
+ * @a: a pointer to an interger
+ * @n: the number of elements of the array
+ *
+ * Return: void
+ */
+void reverse_array(int *a, int n)
+{
+	for (; n-- > 0; a++, n-- )
+	{
+		*a = a[n];
+		a[n] = *a;
+		*a = a[n];
+	}
+}
