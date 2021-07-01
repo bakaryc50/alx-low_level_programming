4b05007bcfd81ae2ec1eb33c4c416d167e8f60a4
Bakaryc50 <bakaryc50@gmail.com>
2021-07-01 05:50:01 -0700

    First attempt: commiting 0x06-pointers_arrays_strings/1-strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+
+/**
+ * _strncat - concatenates two strings
+ * @dest: first pointer parameter to string
+ * @src: second pointer parameter to string
+ * @n: a parameter for n bytes
+ *
+ * Return: a pointer to the resulting string dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	char *dest_end = dest;
+	int src_len = 0;
+
+	while (*dest_end)
+		++dest_end;
+
+	while ((str_len < n) && src[src_len])
+		++src_len;
+
+	if (src + src_len < dest || dest_end + src_len < src)
+	{
+		while (src_len--)
+			*dest_end++ = *src++;
+		*dest_end = '\0';
+	}
+
+	return (dest);
+}
