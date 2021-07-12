2bd4f439ac422eaabe388d6a5683833daa923fc8
Bakaryc50 <bakaryc50@gmail.com>
2021-07-12 10:17:15 -0700

    Fixed Attempt in main code: commiting 0x0A-argc_argv/0-whatsmyname

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,9 +7,8 @@
  *
  * Return: 0 (SUCCESS)
  */
-int main(int argc, char *argv[])
+int main(int argc __attribute__ ((unused)), char *argv[])
 {
-	if (argc == 0)
-		_puts(argv[0]);
+		printf("%s \n", argv[0]);
 	return (0);
 }
