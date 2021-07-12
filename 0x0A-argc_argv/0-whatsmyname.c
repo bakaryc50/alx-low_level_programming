5b7e558939c931d09900129f4ea90e420aa76163
Bakaryc50 <bakaryc50@gmail.com>
2021-07-12 09:29:57 -0700

    First Attempt: commiting 0x0A-argc_argv/0-whatsmyname

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * main - Entry point of the program
+ * @argc: the argument count parameter
+ * @argv: the array of size argc parameter (the pointer to an array of string)
+ *
+ * Return: 0 (SUCCESS)
+ */
+int main(int argc, char *argv[])
+{
+	printf(argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+The **README** file for **0x0A-argc_argv** project
