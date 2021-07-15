5e07e1cfb36322a069032fd9e74d3bc93327f7de
Bakaryc50 <bakaryc50@gmail.com>
2021-07-15 08:49:32 -0700

    Attempt: commiting 0x0C-more_malloc_free/0-malloc_checked

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -2,6 +2,7 @@
 
 /**
  * malloc_checked - allocates memory using malloc
+ * @b: the memory size to allocate
  * Description: if malloc fails, the checker function should cause normal exit
  *
  * Return: a pointer to the allocated memory
@@ -9,6 +10,7 @@
 void *malloc_checked(unsigned int b)
 {
 	void *pointer = malloc(b);
+
 	if (pointer != null)
 		return (pointer);
 	exit(98);
