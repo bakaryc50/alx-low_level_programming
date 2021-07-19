14e0474ef613172063e6f5d9286dfc27eff959da
Bakaryc50 <bakaryc50@gmail.com>
2021-07-19 12:56:31 -0700

    Fixed Attempt: commiting 0x0D-preprocessor/2-main

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - check the code for ALX School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    printf(__FILE__);
+    printf("\n");
+
+    return (0);
+}
