79d0c7096c9d6353e697ff742fac5ed7d55d2e5b
Bakaryc50 <bakaryc50@gmail.com>
2021-07-19 18:29:01 -0700

    Fixed Attempt: commiting 0x0E-structures_typedef/4-new_dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,26 +1,76 @@
 #include "dog.h"
 
 /**
- * new_dog - the function that creates a new do
- * @name: name of the dog
- * @age: age of the dog
- * @owner: owner of the dog
+ * _strdup - create a new array containing a copy of the given string
+ * @str: a pointer to the string to copy
  *
- * Return: a dog, or NULL
+ * Return: NULL if str is NULL or if memory allocation fails,
+ * otherwise a return a pointer to the new copy
+ */
+char *_strdup(char *str)
+{
+	char *dup;
+	unsigned int size = 0;
+
+	if (str)
+	{
+		while (str[size++])
+			;
+
+		dup = malloc(sizeof(char) * size);
+		if (dup)
+		{
+			while (size--)
+				dup[size] = str[size];
+
+			return (dup);
+		}
+	}
+	return (NULL);
+}
+
+/**
+ * new_dog - create a new dog
+ * @name: the new dog's name
+ * @age: the new dog's age
+ * @owner: the new dog's owner
+ *
+ * Return: a pointer to the new dog, or NULL if memory allocation fails
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *new = malloc(dog_t);
+	dog_t *d;
 
-	if (new == NULL)
+	d = malloc(sizeof(dog_t));
+	if (!d)
 		return (NULL);
-	else
+
+	if (name)
 	{
-		new->name = name;
-		new->age = age;
-		new->owner = owner;
+		d->name = _strdup(name);
+		if (!(d->name))
+		{
+			free(d);
+			return (NULL);
+		}
 	}
+	else
+		d->name = NULL;
 
-	return (new);
+	d->age = age;
 
+	if (owner)
+	{
+		d->owner = _strdup(owner);
+		if (!(d->owner))
+		{
+			free(d->name);
+			free(d);
+			return (NULL);
+		}
+	}
+	else
+		d->owner = NULL;
+
+	return (d);
 }
