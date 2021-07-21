7e058be69cf2ed349ff97b1ee90a10f8b48bda0b
Bakaryc50 <bakaryc50@gmail.com>
2021-07-21 11:25:18 -0700

    Fixed Attempt: commiting 0x0F-function_pointer/0-print_name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,22 +1,6 @@
 #include "function_pointers.h"
 
 /**
- * print - that just print a string
- * @n: as a string
- */
-void print(char *n)
-{
-	int i;
-
-	i = 0;
-	while (n[i])
-	{
-		_putchar(n[i]);
-	}
-	_putchar('\n');
-}
-
-/**
  * print_name - a function that prints a name
  * @name: a string parameter
  * @f: is the function pointer
