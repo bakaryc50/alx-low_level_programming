8519faa54c0b6d01a55171e8acbcadf077b12a24
Bakaryc50 <bakaryc50@gmail.com>
2021-07-21 11:29:40 -0700

    Fixed Attempt: commiting 0x0F-function_pointer/0-print_name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -3,10 +3,12 @@
 /**
  * print_name - a function that prints a name
  * @name: a string parameter
- * @f: is the function pointer
+ * @f: is the function pointer to call to print the name
  */
 
 void print_name(char *name, void (*f)(char *))
 {
+	if (!f)
+		return;
 	(f)(name);
 }
