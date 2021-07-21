2a9211748fae5210a4e17ab7d30c400e8954e1e0
Bakaryc50 <bakaryc50@gmail.com>
2021-07-21 14:44:05 -0700

    Fixted Attempt: commiting 0x0F-function_pointers/1-array_iterator

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -4,7 +4,7 @@
  * array_iterator - execute a function given as a parameter on each element
  * @array: an array parameter
  * @size: is the size of the array
- * action: the pointer to the fucntion you need to use
+ * @action: the pointer to the fucntion you need to use
  */
 
 void array_iterator(int *array, size_t size, void (*action)(int))
