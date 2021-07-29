fc476971b98987e006fef1a005ab6edc92c3aa1f
Bakaryc50 <bakaryc50@gmail.com>
2021-07-29 15:33:59 -0700

    Adding : 0x12-singly_linked_lists/1-list_len

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * list_len - returns the number of elements in a linked list_t list
+ * @h: a pointer to the elements list_t
+ *
+ * Return: the number of elements
+ */
+size_t list_len(const list_t *h)
+{
+	if (h != NULL)
+	{
+
+		return (print_list(h->next) + 1);
+	}
+
+	return (0);
+}
