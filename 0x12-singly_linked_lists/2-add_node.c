20007584ba70be11ca579c9228b70b05d40e4460
Bakaryc50 <bakaryc50@gmail.com>
2021-07-29 17:09:26 -0700

    Adding: 0x12-singly_linked_lists/2-add_node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -19,6 +19,34 @@ size_t _strlen(const char *str)
 }
 
 /**
+ * _strdup - create a new array containing a copy of the given string
+ * @str: a pointer to the string to copy
+ *
+ * Return: If str is NULL or if memory allocation fails, return NULL.
+ * Otherwise a return a pointer to the new copy
+ */
+char *_strdup(const char *str)
+{
+	char *dup = NULL;
+	size_t size = 0;
+
+	if (!str)
+		return (NULL);
+
+	while (str[size++])
+		;
+
+	dup = malloc(sizeof(char) * size);
+	if (!dup)
+		return (NULL);
+
+	while (size--)
+		dup[size] = str[size];
+
+	return (dup);
+}
+
+/**
  * add_node - adds a new node at the beginning of a list_t list
  * @head: a pointer to the head of the list
  * @str: the string of the node to add
@@ -34,7 +62,7 @@ list_t *add_node(list_t **head, const char *str)
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
 		return (NULL);
-	new->str = strdup(str);
+	new->str = _strdup(str);
 	new->len = _strlen(str);
 	new->next = *head;
 	*head = new;
