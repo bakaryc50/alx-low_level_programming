39a3fa3caf6711a5a94d676f77b15f47d10bd5ea
Bakaryc50 <bakaryc50@gmail.com>
2021-08-03 12:24:06 -0700

    Fixed commit: 0x13-more_singly_linked_lists/9-insert_node_at_index

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * insert_nodeint_at_index - inserts a new node at a given position
+ * @head: the pointer to the pointer to the node
+ * @idx: the index of the list where the new node should be added starting at 0
+ *
+ * Return: the address of the new node, or NULL if failed
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *new;
+
+	if (*head && idx)
+	{
+		return (insert_nodeint_at_index(&(head)->next, idx - 1, n));
+	}
+	if (!idx)
+		return (NULL);
+	new = malloc(sizeof(listint_t));
+	new->n = n;
+	*head = new;
+
+	return (new);
+}
