4f4299246e326b4a17ac3c1ab95dd0425acbc1bf
Bakaryc50 <bakaryc50@gmail.com>
2021-08-03 12:32:58 -0700

    Fixed commit: 0x13-more_singly_linked_lists/9-insert_node_at_index

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -12,16 +12,16 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *new;
 
-	if (head == NULL)
+	if (!head)
 		return (NULL);
 	if (*head && idx)
 	{
 		return (insert_nodeint_at_index(&(head)->next, idx - 1, n));
 	}
-	if (!idx)
+	if (idx)
 		return (NULL);
 	new = malloc(sizeof(listint_t));
-	if (new == NULL)
+	if (!new)
 		return (NULL);
 	new->n = n;
 	new->next = *head;
