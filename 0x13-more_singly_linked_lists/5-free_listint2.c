ac1d778683407dcb5bc2123cc06c569145308eb4
Bakaryc50 <bakaryc50@gmail.com>
2021-08-03 11:26:21 -0700

    First commit: 0x13-more_singly_linked_lists/5-free_listint2

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - free the linked list
+ * @head: the pointer to the first node
+ * Description: sets the head to NULL
+ */
+void free_listint2(listint_t **head)
+{
+	if (*head)
+		free_listint2(&(*head));
+	*head = NULL;
+}
