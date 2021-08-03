694c447a7d484bd01a11088d0d45d56625251ddb
Bakaryc50 <bakaryc50@gmail.com>
2021-08-03 11:38:15 -0700

    Fixed commit: 0x13-more_singly_linked_lists/5-free_listint2

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -2,12 +2,12 @@
 
 /**
  * free_listint2 - free the linked list
- * @head: the pointer to the first node
+ * @head: the pointer to the pointer to the first node
  * Description: sets the head to NULL
  */
 void free_listint2(listint_t **head)
 {
-	if (*head && head)
+	if (head && *head)
 	{
 		free_listint2(&(*head)->next);
 		free(*head);
