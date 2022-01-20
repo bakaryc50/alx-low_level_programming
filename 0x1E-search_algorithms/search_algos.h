4bd0471a85a3f23c06dc77ae540722525310c6ca
Bakaryc50 <bakaryc50@gmail.com>
2022-01-20 11:11:37 -0800

    Bakary commit: 1-binary.c

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -6,8 +6,6 @@
 
 
 int linear_search(int *array, size_t size, int value);
-static void print_array(int *array, size_t lo, size_t hi);
-static int _binary_search(int *array, size_t lo, size_t hi, int value);
 int binary_search(int *array, size_t size, int value);
 
 #endif /* SEARCH_ALGOS_H */
